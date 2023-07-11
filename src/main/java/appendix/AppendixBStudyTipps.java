89ab3ed2983143104c640611f9b165d7a9537a95
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-11 17:28:06 +0300

    appendix B study tipps - e.g. with code that does not compile

---
+++ src/main/java/appendix/AppendixBStudyTipps.java
@@ -0,0 +1,13 @@
+package appendix;
+
+public class AppendixBStudyTipps {
+
+    //float value = 102.0; // DOES NOT COMPILE
+
+    float value1 = 102;
+    float value2 = (int)102.0;
+    //float value3 = 1f * 0.0; // DOES NOT COMPILE
+    float value4 = 1f * (short)0.0;
+    //float value5 = 1f * (boolean)0; // DOES NOT COMPILE
+
+}

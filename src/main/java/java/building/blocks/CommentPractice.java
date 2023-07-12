cad9b43a7c0e369041379a86ff1e9de79084ac46
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-12 09:37:12 +0300

    chapter1 - comments

---
+++ src/main/java/java/building/blocks/CommentPractice.java
@@ -0,0 +1,21 @@
+package java.building.blocks;
+
+public class CommentPractice {
+
+    /*
+     * // anteater --> multiline comment
+     */
+
+    // bear --> single line comment
+
+    // // cat --> single line comment
+
+    // /* dog */ --> single line comment
+
+    /* elephant */ //--> multiline comment
+
+    /*
+     * /* ferret */
+ // */  --> this is additional, it won't compile!!!
+
+}
---
+++ src/main/java/java/building/blocks/CommentType.java
@@ -0,0 +1,16 @@
+package java.building.blocks;
+
+public class CommentType {
+
+    //single-line comment
+
+
+    /*
+    (*optional) multiple-line comment | multiline comment
+     */
+
+    /**
+     * Javadoc comment
+     * @autor Scott and Jeanne
+     */
+}

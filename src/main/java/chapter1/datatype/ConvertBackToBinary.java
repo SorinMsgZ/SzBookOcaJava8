6ac51e25d035119720cbfbf78b220125ca625ea3
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-13 17:04:45 +0300

    chapter1 - practice primitive types

---
+++ src/main/java/chapter1/datatype/ConvertBackToBinary.java
@@ -0,0 +1,11 @@
+package chapter1.datatype;
+//literal = a number which is present in the code using different type of base
+public class ConvertBackToBinary {
+
+    public static void main(String[] args) {
+        System.out.println(56);       // decimal base (base 10: digits 0–9) -> 56
+        System.out.println(0b11);     // binary base (base 2: digits 0–1) -> 3
+        System.out.println(017);      // octal base (base 8: digits 0–7) -> 15
+        System.out.println(0x1F);     // hexadecimal base (base 16: digits 0–9 and letters A–F) -> 31
+    }
+}

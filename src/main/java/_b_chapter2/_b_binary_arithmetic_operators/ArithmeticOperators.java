3840b19fe420183aaac8438e430d7d435fb99fa6
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-18 15:04:10 +0300

    chapter2 - ninary arithmetic operators

---
+++ src/main/java/_b_chapter2/_b_binary_arithmetic_operators/ArithmeticOperators.java
@@ -0,0 +1,11 @@
+package _b_chapter2._b_binary_arithmetic_operators;
+
+public class ArithmeticOperators {
+
+    /*
+      addition (+),
+      subtraction (-),
+      multiplication (*),
+      division (/),
+      and modulus (%)*/
+}

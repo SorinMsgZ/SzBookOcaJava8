1d3a30ac9bc99357be498c669e95596bf4ff3e8b
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-18 15:04:49 +0300

    chapter2 - unary operators

---
+++ src/main/java/_b_chapter2/_c_unary_operators/UnaryOperators.java
@@ -0,0 +1,19 @@
+package _b_chapter2._c_unary_operators;
+
+public class UnaryOperators {
+
+    /*TA B LE 2 . 2 Java unary operators
+
+    Unary operator Description
+
+        + Indicates a number is positive, although numbers are assumed to be positive in Java
+        unless accompanied by a negative unary operator
+
+        - Indicates a literal number is negative or negates an expression
+
+        ++ Increments a value by 1
+
+        -- Decrements a value by 1
+
+         ! Inverts a Boolean’s logical value*/
+}

db1f7fad2535c49b3477be13f891a19600b492ff
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-18 15:03:22 +0300

    chapter2 - operators

---
+++ src/main/java/_b_chapter2/_a_java_operators/JavaOperators.java
@@ -0,0 +1,46 @@
+package _b_chapter2._a_java_operators;
+
+public class JavaOperators {
+
+   /* TABLE 2.1 Order of operator precedence
+    Operator Symbols and examples
+
+        //    Post-unary operators
+            expression++, expression--
+
+        //    Pre-unary operators
+            ++expression, --expression
+
+        //    Other unary operators
+            +, -, !
+
+        //    Multiplication/Division/Modulus
+            *, /, %
+
+        //    Addition/Subtraction
+            +, -
+
+        //    Shift operators
+            <<, >>, >>>
+
+        //    Relational operators
+            <, >, <=, >=, instanceof
+
+        //    Equal to/not equal to
+            ==, !=
+
+        //    Logical operators
+            &, ^, |
+
+        //    Short-circuit logical operators
+            &&, ||
+
+        //    Ternary operators
+            boolean expression ? expression1 : expression2
+
+        //    Assignment operators
+            =, +=, -=, *=, /=, %=, &=, ^=, !=, <<=, >>=, >>>=
+
+
+    */
+}

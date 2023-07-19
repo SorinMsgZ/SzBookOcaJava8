16c95cc4e4ef2a6f30905c24e11e1354beed46a2
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-19 18:06:33 +0300

    chapter1 - further default initialization

---
+++ src/main/java/_a_chapter1/_g_default_initialization_of_variables/CharDefaultInitialization.java
@@ -0,0 +1,10 @@
+package _a_chapter1._g_default_initialization_of_variables;
+
+public class CharDefaultInitialization {
+    char c;
+    public static void main(String[] args) {
+        CharDefaultInitialization test = new CharDefaultInitialization();
+
+        System.out.println(test.c); //returns NUL
+    }
+}
---
+++ src/main/java/_a_chapter1/_g_default_initialization_of_variables/StringDefaultInitialization.java
@@ -0,0 +1,10 @@
+package _a_chapter1._g_default_initialization_of_variables;
+
+public class StringDefaultInitialization {
+    String c;
+    public static void main(String[] args) {
+        StringDefaultInitialization test = new StringDefaultInitialization();
+
+        System.out.println(test.c); //returns NULL
+    }
+}

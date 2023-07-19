8bfdede599a2a3a2d0c4ddab24b79a7877a45bb1
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-19 18:09:06 +0300

    chapter2 - review questions

---
+++ src/main/java/_b_chapter2/_z_review_question/ReviewQuestion.java
@@ -0,0 +1,11 @@
+package _b_chapter2._z_review_question;
+
+public class ReviewQuestion {
+    public static void main(String[] args) {
+        int x1 = 50, x2 = 75;
+        boolean b = x1 >= x2;
+        if (b = true)
+            System.out.println("Success");
+        else System.out.println("Failure");
+    }
+}

b1d225d58a0caf30aca47073f6643ed7815e4846
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-07-20 14:38:53 +0300

    chapter2 - + practice

---
+++ src/main/java/_b_chapter2/_a_java_operators/PlusTest.java
@@ -0,0 +1,17 @@
+package _b_chapter2._a_java_operators;
+
+public class PlusTest {
+    public static void main(String[] args) {
+        int x=1;
+        String s1 = "a";
+
+        String n = null;
+        System.out.println(+x);
+        System.out.println(s1+n);
+
+        String s2 = "x";
+
+        s2 +="yz";
+        System.out.println(s2);
+    }
+}

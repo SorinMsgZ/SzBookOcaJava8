988810710e3b389ef9a2e17d43c1a7d32e3fb4c9
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-08-01 09:46:59 +0300

    chapter3 - multidimensional arrays

---
+++ src/main/java/_c_chapter3/_d_multidimensional_array/CreatingMultidimArray.java
@@ -0,0 +1,27 @@
+package _c_chapter3._d_multidimensional_array;
+
+public class CreatingMultidimArray {
+
+    int[][] vars1;          // 2D array
+    int vars2 [][];         // 2D array
+    int[] vars3[];          // 2D array
+    int[] vars4 [], space [][];  // a 2D AND a 3D array
+
+
+
+    //asymmetric array - mode 1
+    int[][] differentSize = {{1, 4}, {3}, {9,8,7}};
+
+    public static void main(String[] args) {
+
+        CreatingMultidimArray m = new CreatingMultidimArray();
+        System.out.println(m.vars1.length); //NullPointerException
+
+
+        //asymmetric array - mode 2
+        int [][] argsx = new int[4][];
+        argsx [0] = new int[5];
+        argsx [1] = new int[3];
+
+    }
+}

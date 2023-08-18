bf604b98f8e0ecf3c8eb800297351d7ee1a8f094
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-08-18 16:21:30 +0300

    chapter5 - abstract classes

---
+++ src/main/java/_e_chapter5/_x_abstract_classes/CreatingAbstractClass.java
@@ -0,0 +1,10 @@
+package _e_chapter5._x_abstract_classes;
+
+public abstract class CreatingAbstractClass {
+    protected int age;
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+    public abstract String getName();
+
+}

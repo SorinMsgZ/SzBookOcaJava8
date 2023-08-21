068fbf0e26e8f93e42eb7d120126a33793ec3df9
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-08-21 09:47:46 +0300

    chapter5 - inheritance - multiple inheritance

---
+++ src/main/java/_e_chapter5/_a_class_inheritance/_i_multiple_inheritance/MultipleInheritanceNotAllowed.java
@@ -0,0 +1,7 @@
+package _e_chapter5._a_class_inheritance._i_multiple_inheritance;
+
+/*public class MultipleInheritanceNotAllowed extends ParentAnimal, ParentPet { //compile error when multiple parents
+}*/
+
+public class MultipleInheritanceNotAllowed extends ParentAnimal { //just one single parent
+}

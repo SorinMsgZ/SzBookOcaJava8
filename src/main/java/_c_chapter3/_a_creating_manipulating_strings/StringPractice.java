dbed4df2248895f23848ad2279847647d797b0ae
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-09-05 14:30:56 +0300

    chapter3 committed in Eclipse

--- src/main/java/_c_chapter3/_a_creating_manipulating_strings/StringPractice.java
+++ src/main/java/_c_chapter3/_a_creating_manipulating_strings/StringPractice.java
@@ -2,10 +2,13 @@ package _c_chapter3._a_creating_manipulating_strings;
 
 public class StringPractice {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String s = "0123456789";
+		String s = "0123456789";
 
-        System.out.println(s.length());
-    }
+		System.out.println(s.length());
+		System.out.println(s.charAt(10));
+		System.out.println(s.charAt(9));
+
+	}
 }

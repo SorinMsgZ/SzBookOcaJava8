f0ae8b86c65837e47b00c44d8263de32835c8d26
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-09-05 11:56:29 +0300

    chapter5 - etc

---
+++ src/main/java/_e_chapter5/_c_interfaces/DefaultMethods.java
@@ -0,0 +1,27 @@
+package _e_chapter5._c_interfaces;
+
+public class DefaultMethods implements FirstInterface, SecondInterface{
+
+    @Override
+    public void defaultMethod() {
+        FirstInterface.super.defaultMethod();
+
+    }
+}
+
+interface FirstInterface{
+    int var =1;
+    default void defaultMethod(){
+        System.out.println("interface1");
+    }
+    default void otherDefaultMethod(){
+        System.out.println("interface1 other method");
+    }
+}
+
+interface SecondInterface{
+
+    default void defaultMethod(){
+        System.out.println("interface2");
+    }
+}

3fbd2a28af84a86893399e82cd49bff824348829
SorinMsgZ <sorin.zdrenghia@msg.group>
2023-09-11 09:27:37 +0300

    etc

--- src/main/java/_d_chapter4/_i_predicates/OtherPredicatePractice.java
+++
@@ -1,9 +0,0 @@
-package _d_chapter4._i_predicates;
-
-
-
-public class OtherPredicatePractice {
-    public static void main(String[] args) {
-       // System.out.println(""df"); //does not compile
-    }
-}
